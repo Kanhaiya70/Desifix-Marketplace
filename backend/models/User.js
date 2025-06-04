ae8416d9a1a00c6c6a65b3c07f7827916f8e4e3e
Kanhaiya70 <Kanhaiyaag964@gmail.com>
2025-06-05 01:18:28 +0530

    Add basic authentication

---
+++ backend/models/User.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name : String,
+    email : { type : String, unique : true },
+    password : String,
+    role : { type : String, enum : ['user', 'provider', 'admin'], default : 'user'}
+});
+
+module.exports = mongoose.model('User', userSchema);
